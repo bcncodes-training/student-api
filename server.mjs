c436c9dd0a54d76f85338bd0f9875925f015370d
raul g <raulg@mail.com>
2019-11-04 13:36:26 +0100

    process.env configuration added

--- server.mjs
+++ server.mjs
@@ -1,13 +1,14 @@
 import https from 'https';
 import app from './app.mjs';
 import fs from 'fs';
-import path from 'path';
+
+
 
 https.createServer({
     key: fs.readFileSync( './config/server.key'),
     cert: fs.readFileSync( './config/server.cert')
 }, app)
-.listen(8443, _=> {
+.listen(process.env.PORT, _=> {
     console.log('Example app listening on port 8443! Go to https://localhost:8443/') });
     
    
